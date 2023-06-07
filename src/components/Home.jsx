3d910e3c6e3f11b1fd5cad65b5eae732a8fc55f7
Athif Binu <binuathif@gmail.com>
2023-06-08 00:24:59 +0530

    err fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,109 +1,120 @@
-import React, { useEffect, useState } from 'react'
-import "../Styles/Home.css"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "../Styles/Home.css";
+import axios from "axios";
 
+import { ENV } from "../utlits/Constants";
 
 const Home = () => {
+  const [characters, setCharacters] = useState([]);
 
-  const [data,setData]=useState(null)
+
+   const [Search,setSearch]=useState()
+
+  useEffect(() => {
+    fetchCharecters();
+  }, []);
+
+  const fetchCharecters = async () => {
+    axios
+      .get(`${ENV.URL}/character`, {
+        headers: {
+          Authorization: `Bearer ${ENV.TOKEN}`,
+        },
+      })
+      .then((res) => {
+        const data = res.data.docs;
+
+        console.log("res", data);
+
+        setCharacters(data);
+      
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  const handleSearch=(event)=>{
+    const searchValue = event.target.value;
+   
+     
+ 
+  }
 
 
-  useEffect(()=>{
-    const fetchDate=async()=>{
-      try{
-        const responce=await axios.get('https://the-one-api.dev/v2/character')
-        setData(responce.data)
-      }catch(err){
-        console.log(err)
-      }
-    }
 
-    fetchDate();
-  },[]);
-
+  
 
   return (
-      <div className="container">
-        <div className="search-form">
+    <div className="container">
+      <div className="search-form">
         <div className="input-line">
           <label>Serach</label>
-        <input type="text" className="input-box" />
-        <label >Sort By</label>
-        <input type="text" className="input-box" />
-      </div>
-      <div className="input-line">
-        <label >Race</label>
-        <input type="text" className="input-box" />
-        <label>Gender</label>
-        <input type="text" className="input-box" />
-               <button>Submit</button>
-      </div>
-
+          <input type="text" className="input-box" />
+          <label>Sort By</label>
+          <input type="text" className="input-box" />
         </div>
-
-
-         <table className='table'>
-
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Name</th>
-              <th>Race</th>
-              <th>Gender</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <td>1</td>
-            <td>Athif</td>
-            <td>Human</td>
-            <td>Male</td>
-            <td>Detailes</td>
-          </tbody>
-
-          <tbody>
-            <td>1</td>
-            <td>Athif</td>
-            <td>Human</td>
-            <td>Male</td>
-            <td>Detailes</td>
-          </tbody>
-
-          <tbody>
-            <td>1</td>
-            <td>Athif</td>
-            <td>Human</td>
-            <td>Male</td>
-            <td>Detailes</td>
-          </tbody>
-
-
-         </table>
-
-
-         <div className="footer-section">
-             <hr />
-             <div className="footer-content">
-              <button>1</button>
-              <button>2</button>
-              <button>9</button>
-
-              <div className="limit-section">
-                <label>Limit</label>
-                <button>10</button>
-              </div>
-             </div>
-             <hr />
-         </div>
-
-
-         
-
-
+        <div className="input-line">
+          <label>Race</label>
+          <input type="text" className="input-box" />
+          <label>Gender</label>
+          <input type="text" className="input-box" />
+          <button>Submit</button>
+        </div>
       </div>
-    
-  )
-}
 
-export default Home
+      <table className="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Race</th>
+            <th>Gender</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <td>1</td>
+          <td>Athif</td>
+          <td>Human</td>
+          <td>Male</td>
+          <td>Detailes</td>
+        </tbody>
+
+        <tbody>
+          <td>1</td>
+          <td>Athif</td>
+          <td>Human</td>
+          <td>Male</td>
+          <td>Detailes</td>
+        </tbody>
+
+        <tbody>
+          <td>1</td>
+          <td>Athif</td>
+          <td>Human</td>
+          <td>Male</td>
+          <td>Detailes</td>
+        </tbody>
+      </table>
+
+      <div className="footer-section">
+        <hr />
+        <div className="footer-content">
+          <button>1</button>
+          <button>2</button>
+          <button>9</button>
+
+          <div className="limit-section">
+            <label>Limit</label>
+            <button>10</button>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/utlits/Constants.js
@@ -0,0 +1,7 @@
+
+
+export const ENV={
+    URL:"https://the-one-api.dev/v2",
+    TOKEN:"VYB7tQJRU7dvaMQvSAmD",
+
+}
