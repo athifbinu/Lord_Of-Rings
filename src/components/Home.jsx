469455537a4a81daed69acfa79a6ab1371897b6f
Athif Binu <binuathif@gmail.com>
2023-06-07 20:18:27 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../Styles/Home.css"
+import axios from 'axios'
 
 
 const Home = () => {
+
+  const [data,setData]=useState(null)
+
+
+  useEffect(()=>{
+    const fetchDate=async()=>{
+      try{
+        const responce=await axios.get('https://the-one-api.dev/v2/character')
+        setData(responce.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+    fetchDate();
+  },[]);
+
+
   return (
-    <div className="container">
-      <div className="search-box">
-        <p>Search</p>
-        <input type="text" placeholder='By Name' />
+      <div className="container">
+        <div className="search-form">
+        <div className="input-line">
+          <label>Serach</label>
+        <input type="text" className="input-box" />
+        <label >Sort By</label>
+        <input type="text" className="input-box" />
+      </div>
+      <div className="input-line">
+        <label >Race</label>
+        <input type="text" className="input-box" />
+        <label>Gender</label>
+        <input type="text" className="input-box" />
+               <button>Submit</button>
       </div>
 
-      <div className="search-box">
-        <p>Sort By</p>
-        <input type="text" placeholder='By Name(ass/desc)' />
-      </div>
-
-      <div className="search-box">
-        <p>Race</p>
-        <input type="text" placeholder='List Of Race' />
-      </div>
-
-      <div className="search-box">
-        <p>Gender</p>
-        <input type="text" placeholder='Male/Female' />
-      </div>
-
-        <button>Submit</button>
-
-
-        {/* finalResult */}
-        <div className="search-result">
-             <div className="content">
-              
-             </div>
         </div>
 
-       
-    </div>
+
+         <table className='table'>
+
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Race</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <td>1</td>
+            <td>Athif</td>
+            <td>Human</td>
+            <td>Male</td>
+            <td>Detailes</td>
+          </tbody>
+
+          <tbody>
+            <td>1</td>
+            <td>Athif</td>
+            <td>Human</td>
+            <td>Male</td>
+            <td>Detailes</td>
+          </tbody>
+
+          <tbody>
+            <td>1</td>
+            <td>Athif</td>
+            <td>Human</td>
+            <td>Male</td>
+            <td>Detailes</td>
+          </tbody>
+
+
+         </table>
+
+
+         <div className="footer-section">
+             <hr />
+             <div className="footer-content">
+              <button>1</button>
+              <button>2</button>
+              <button>9</button>
+
+              <div className="limit-section">
+                <label>Limit</label>
+                <button>10</button>
+              </div>
+             </div>
+             <hr />
+         </div>
+
+
+         
+
+
+      </div>
     
   )
 }
