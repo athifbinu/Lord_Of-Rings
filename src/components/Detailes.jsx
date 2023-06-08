6dde5c690b9e0af38885e3ee18d665778d91d9c1
Athif Binu <binuathif@gmail.com>
2023-06-08 14:05:48 +0530

    fix

--- src/components/Detailes.jsx
+++ src/components/Detailes.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import "../Styles/Detailes.css"
+import { Link } from 'react-router-dom'
 
 const Detailes = () => {
   return (
-      <div className='main-nav'>
-           <div className='left-section'>
+    <>
+    <div className='hero-section'>
+       <div className='main-section'>
+        <div className='left-section'>
                  <p>Name</p>
+                 <p>WikiUrl</p>
+                 <p>Race</p>
+                 <p>Gender</p>
+                 <p>Height</p>
+                 <p>Hair</p>
+                 <p>Realm</p>
+                 <p>Birth</p>
+                 <p>Spouse</p>
+                 <p>Death</p>
            </div>
 
-           <div className='right-section'>
-                 <p>Athif</p>
-           </div>
+        <div className='right-section'>
+           <p>Athif</p>
+         </div>
+
+          
       </div>
+
+       <div className='close-btn'>
+
+          <Link to="/home">
+          <button>Close</button>
+          </Link>
+       </div>
+       
+    </div>
+     <hr/>
+  </>
+
   )
 }
 
