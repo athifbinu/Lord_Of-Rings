3028a515e3137c50325007d207df4c2993b3b375
Athif Binu <binuathif@gmail.com>
2023-06-09 11:22:02 +0530

    finish

--- src/components/Detailes.jsx
+++ src/components/Detailes.jsx
@@ -1,62 +1,81 @@
-import React, { useEffect, useState } from 'react'
-import "../Styles/Detailes.css"
-import { Link, useParams } from 'react-router-dom'
-import axios from 'axios';
-import { ENV } from '../utlits/Constants';
+import React, { useEffect, useState } from "react";
+import "../Styles/Detailes.css";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { ENV } from "../utlits/Constants";
 
 const Detailes = () => {
+  const { _id } = useParams();
+  console.log("id", _id);
 
 
-   const {_id}=useParams();
-   console.log("id",_id)
+  const [cherecterId,setCharecterId]=useState([])
 
 
-   const getProductId=async(id)=>{
-      const responce=await axios.get(`${ENV.URL}/character/${id}`)
+  useEffect(() => {
+   fetchCharecters();
+ }, []);
 
-      return responce.data;
-   }
-  
+  const fetchCharecters = async () => {
+   axios
+     .get(`${ENV.URL}/character`, {
+       headers: {
+         Authorization: `Bearer ${ENV.TOKEN}`,
+       },
+     })
+     .then((res) => {
+       const data = res.data.docs;
+
+       setCharecterId(data);
+       setCharecterId(data);
+
+       console.log(data,"data")
+     })
+     .catch((err) => {});
+ };
 
 
+ 
+
+
+
+
+
+
+ 
 
 
   return (
     <>
-    <div className='hero-section'>
-       <div className='main-section'>
-        <div className='left-section'>
-                 <p>Name</p>
-                 <p>WikiUrl</p>
-                 <p>Race</p>
-                 <p>Gender</p>
-                 <p>Height</p>
-                 <p>Hair</p>
-                 <p>Realm</p>
-                 <p>Birth</p>
-                 <p>Spouse</p>
-                 <p>Death</p>
-           </div>
+      <div className="hero-section">
+        <div className="main-section">
+          <div className="left-section">
+            <p>Name</p>
+            <p>WikiUrl</p>
+            <p>Race</p>
+            <p>Gender</p>
+            <p>Height</p>
+            <p>Hair</p>
+            <p>Realm</p>
+            <p>Birth</p>
+            <p>Spouse</p>
+            <p>Death</p>
+          </div>
 
-        <div className='right-section'>
-           <p>Athif</p>
-         </div>
+          <div className="right-section">
+            <p>Athif</p>
+          </div>
+        </div>
 
-          
-      </div>
-
-       <div className='close-btn'>
-
+        <div className="close-btn">
           <Link to="/home">
-          <button>Close</button>
+            <button>Close</button>
           </Link>
-       </div>
-       
-    </div>
-     <hr/>
-  </>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+};
 
-  )
-}
-
-export default Detailes
+export default Detailes;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,168 +3,160 @@ import "../Styles/Home.css";
 import axios from "axios";
 import { ENV } from "../utlits/Constants";
 
-
-
 import AutoCard from "./AutoCard";
 
 import { Link } from "react-router-dom";
-const Home = () => {
 
+const Home = () => {
   const [characters, setCharacters] = useState([]);
 
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
-  const [search, setSeachName] = useState('');
+  const [search, setSeachName] = useState("");
   const [filterarray, setFilterArray] = useState([]);
   const [gender, setGender] = useState(null);
-  const [sort, setSort] = useState('asc');
+  const [sort, setSort] = useState("asc");
 
-
-  const [selectedRaces, setSelectedRaces] = useState([])
-
+  const [selectedRaces, setSelectedRaces] = useState([]);
 
   useEffect(() => {
-
     fetchCharecters();
-  },[limit,page]);
+  }, [limit, page]);
 
-
-   useEffect(()=>{
-      let sortedcharecteres=characters.sort((a, b)=>{
-        if(sort === "asc"){
-          if (a.name < b.name) {
-            return -1;
-          }
-          if (a.name > b.name) {
-            return 1;
-          }
-          return 0;
-        }else if(sort === 'dsc'){
-        if(a.name > b.name) {
-            return -1;
-          }
-          if (a.name < b.name) {
-            return 1;
-          }
-          return 0;
+  useEffect(() => {
+    let sortedcharecteres = characters.sort((a, b) => {
+      if (sort === "asc") {
+        if (a.name < b.name) {
+          return -1;
         }
-        });
-        setCharacters(sortedcharecteres)
-   },[sort])
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      } else if (sort === "dsc") {
+        if (a.name > b.name) {
+          return -1;
+        }
+        if (a.name < b.name) {
+          return 1;
+        }
+        return 0;
+      }
+    });
+    setCharacters(sortedcharecteres);
+  }, [sort]);
   const fetchCharecters = async () => {
-
     axios
       .get(`${ENV.URL}/character`, {
         headers: {
           Authorization: `Bearer ${ENV.TOKEN}`,
-          
-          
         },
-        params:{
-          limit:limit,
-          page:page,
-          gender:gender!=='any'?gender:null,
-
-        }
-        
+        params: {
+          limit: limit,
+          page: page,
+          gender: gender !== "any" ? gender : null,
+        },
       })
       .then((res) => {
         const data = res.data.docs;
 
-        
-
         setCharacters(data);
-        setFilterArray(data)
-      
+        setFilterArray(data);
       })
-      .catch((err) => {
-      
-      });
+      .catch((err) => {});
   };
 
-  console.log()
+  console.log();
 
+  const handleSearch = (e) => {
+    const SerachTerm = e.target.value;
+    setSeachName(SerachTerm);
+    const filtered = characters.filter((item) =>
+      item.name.toLowerCase().includes(SerachTerm.toLowerCase())
+    );
+    setFilterArray(filtered);
+  };
 
-  const handleSearch=e=>{
-    const SerachTerm=e.target.value;
-       setSeachName(SerachTerm)
-    const filtered=characters.filter(item =>
-      item.name.toLowerCase().includes(SerachTerm.toLowerCase()))
-       setFilterArray(filtered)
-      
-  }
-
-  const onpressSubmitbtn=()=>{
-       
-    fetchCharecters()
-  }
-  
+  const onpressSubmitbtn = () => {
+    fetchCharecters();
+  };
 
   const handleRaceSelect = (value) => {
-    setSelectedRaces(value.map(option => option.title));
-    setFilterArray(characters.filter(character => value.map(option => option.title)
-    .includes(character.race)).length!==0?characters.filter(character => value.map(option => option.title).includes(character.race)):characters);
-  
-  }
+    setSelectedRaces(value.map((option) => option.title));
+    setFilterArray(
+      characters.filter((character) =>
+        value.map((option) => option.title).includes(character.race)
+      ).length !== 0
+        ? characters.filter((character) =>
+            value.map((option) => option.title).includes(character.race)
+          )
+        : characters
+    );
+  };
 
-
-
-   const renderCharecterItem=(item,index)=>{
-    return  (
-      <tbody >
-      <td>{index+1}</td>
-      <td>{item.name}</td>
-      <td>{item.race}</td>
-      <td>{item.gender}</td>
-      <Link className="th-link" to={`/detailes/${item._id}`}><td>Details <span>{`>`}</span></td></Link>
-    </tbody>
-    )
-   }
-
-
+  const renderCharecterItem = (item, index) => {
+    return (
+      <tbody>
+        <td>{index + 1}</td>
+        <td>{item.name}</td>
+        <td>{item.race}</td>
+        <td>{item.gender}</td>
+        <Link className="th-link" to={`/detailes/${item._id}`}>
+          <td>
+            Details <span>{`>`}</span>
+          </td>
+        </Link>
+      </tbody>
+    );
+  };
 
   return (
     <div className="container">
-
       <div className="search-form">
         <div className="input-line">
           <label>Search</label>
-          <input value={search} onChange={(e)=>handleSearch(e)} type="text" className="input-box" placeholder="by name"  />
+          <input
+            value={search}
+            onChange={(e) => handleSearch(e)}
+            type="text"
+            className="input-box"
+            placeholder="by name"
+          />
 
-
-           <div className="dropdown-container">
-           <label>Sort By</label>
-           <select value={sort} onChange={(event)=>setSort(event.target.value)} >
-                 
-                 <option value="asc">asc</option>
-                 <option value="dsc">dsc</option>
-           </select>
-
-           </div>
-
+          <div className="dropdown-container">
+            <label>Sort By</label>
+            <select
+              value={sort}
+              onChange={(event) => setSort(event.target.value)}
+            >
+              <option value="asc">asc</option>
+              <option value="dsc">dsc</option>
+            </select>
+          </div>
         </div>
-        
+
         <div className="input-line">
+          <AutoCard handle={(v) => handleRaceSelect(v)} />
 
-          <AutoCard handle={(v)=>handleRaceSelect(v)}/>
+          <div className="dropdown-container">
+            <label>Gender</label>
+            <select
+              value={gender}
+              onChange={(event) => setGender(event.target.value)}
+            >
+              <option value="any">any</option>
+              <option value="Male">male</option>
+              <option value="Female">female</option>
+            </select>
+          </div>
 
-       
-           <div className="dropdown-container">
-           <label>Gender</label>
-           <select value={gender} onChange={(event)=>setGender(event.target.value)} >
-                 <option value="any">any</option>
-                 <option value="Male">male</option>
-                 <option value="Female">female</option>
-             
-           </select>
-
-           </div>
-      
-          <button onClick={onpressSubmitbtn} className="search-btn">Submit</button>
+          <button onClick={onpressSubmitbtn} className="search-btn">
+            Submit
+          </button>
         </div>
       </div>
 
-       <hr/>
+      <hr />
 
       <table className="table">
         <thead>
@@ -176,41 +168,34 @@ const Home = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        
-      {
-        filterarray.map((item,index)=>{
-      
-         return renderCharecterItem(item,index)
-      })
-      }
 
-
-
+        {filterarray.map((item, index) => {
+          return renderCharecterItem(item, index);
+        })}
       </table>
-      
-        <hr/>
+
+      <hr />
       <div className="footer-section">
-           <div className="footer-btn" style={{display:"flex"}}>
-                <button onClick={()=>setPage( page!==1?page-1:1)}>{`<`}</button>
-               <div>{page}</div>
-               <button onClick={()=>setPage(page+1)} >{`>`}</button>
-           </div>
+        <div className="footer-btn" style={{ display: "flex" }}>
+          <button
+            onClick={() => setPage(page !== 1 ? page - 1 : 1)}
+          >{`<`}</button>
+          <div>{page}</div>
+          <button onClick={() => setPage(page + 1)}>{`>`}</button>
+        </div>
 
-        
-          <div className="limit-dropdown">
-           <label>Limit</label>
-              <select value={limit} onChange={(event)=>setLimit(event.target.value)} >
-                 
-                <option value="10">10</option>
-                <option value="50">50</option>
-              </select>
-
-           </div>
-          
-        
-   
+        <div className="limit-dropdown">
+          <label>Limit</label>
+          <select
+            value={limit}
+            onChange={(event) => setLimit(event.target.value)}
+          >
+            <option value="10">10</option>
+            <option value="50">50</option>
+          </select>
+        </div>
       </div>
-      <hr/>
+      <hr />
     </div>
   );
 };
